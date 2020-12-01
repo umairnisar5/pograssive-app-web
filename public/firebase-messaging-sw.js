9faf23d9b44ca01730c89f9a3cf5dbd3bbc02f13
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-12-02 04:18:02 +0500

    create a firebase config

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCHoaR7lYPLwyu0SL2N3Mp4mynuVgIj2Ls",
+    authDomain: "notifactionpp.firebaseapp.com",
+    databaseURL: "https://notifactionpp.firebaseio.com",
+    projectId: "notifactionpp",
+    storageBucket: "notifactionpp.appspot.com",
+    messagingSenderId: "564411562482",
+    appId: "1:564411562482:web:39c481d44467578206936b"
+  };
