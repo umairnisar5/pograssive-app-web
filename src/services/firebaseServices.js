5e1df1b8578cf9156b9adfc4eb19d3415b237f32
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-12-02 04:56:38 +0500

    create a configure Notification

--- src/services/firebaseServices.js
+++ src/services/firebaseServices.js
@@ -1,3 +1,30 @@
-export function configureNotification(){
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCHoaR7lYPLwyu0SL2N3Mp4mynuVgIj2Ls",
+    authDomain: "notifactionpp.firebaseapp.com",
+    databaseURL: "https://notifactionpp.firebaseio.com",
+    projectId: "notifactionpp",
+    storageBucket: "notifactionpp.appspot.com",
+    messagingSenderId: "564411562482",
+    appId: "1:564411562482:web:39c481d44467578206936b"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const messaging = firebase.messaging();
+
+export function initNotification() {
     Notification.requestPermission().then((Permission) => {console.log(Permission)})
-}
+    if(Permission === "granted") {
+    messaging.getToken().then((currentToken) => {
+        if (currentToken) {
+         console.log("TOKEN => ", currentToken);
+        } else {
+          console.log('No registration token available. Request permission to generate one.');
+         
+        }
+      }).catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+       
+      });
+} }
